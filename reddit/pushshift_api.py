22dd302d35bce5877288fa95aabfd36b9db901b2
Shamama Sirroon <ssirroon@olin.edu>
2022-03-28 17:38:08 -0400

    Reddit posts with stock names

--- reddit/pushshift_api.py
+++ reddit/pushshift_api.py
@@ -10,13 +10,13 @@ def get_posts_for_time_period(sub, beginning, end=int(datetime.datetime.now(
 ).timestamp())):
     """
     Gets posts from the given subreddit for the given time period in json form.
-    
+
     Args:
         sub: A string representing the subreddit to retrieve posts from.
         beginning: The unix timestamp of when the posts should begin.
         end: The unix timestamp of when the posts should end (defaults to
         current time).
-    
+
     Returns:
     """
     print("Querying pushshift")
@@ -32,9 +32,8 @@ def get_posts_for_time_period(sub, beginning, end=int(datetime.datetime.now(
 
 
 beginning_timestamp = int(datetime.datetime(
-    year=2018, month=1, day=1).timestamp())  # + i
-end_timestamp = int(datetime.datetime(year=2019, month=1, day=1).timestamp()
-                    )  # + 1 + i
+    year=2018, month=1, day=1).timestamp())
+end_timestamp = int(datetime.datetime(year=2019, month=1, day=1).timestamp())
 data = get_posts_for_time_period(
     "wallstreetbets", beginning_timestamp, end_timestamp)
 all_data = data
@@ -58,11 +57,11 @@ for post in all_data:
     # print(str(datetime.datetime.fromtimestamp(post['data']['created'])))
     # if "2018" in str(datetime.datetime.fromtimestamp(post['data']['created'])):
     # print(datetime.datetime.fromtimestamp(post['data']['created']))
-    if 'selftext' in post:
+    if 'selftext' in post and "$" in post['selftext'] or "$" in post['title']:
         df = pd.concat(
             [df, pd.DataFrame({'subreddit': [post['subreddit']],
-                               'title': [pf.sensor(post['title'])],
-                               'selftext': [pf.sensor(post['selftext'])],
+                               'title': [pf.censor(post['title'])],
+                               'selftext': [pf.censor(post['selftext'])],
                                'time': [datetime.datetime.fromtimestamp
                                         (post['created_utc'])]})])
 df.to_csv("reddit/reddit_posts.csv")
