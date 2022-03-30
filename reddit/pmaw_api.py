306879c8b5c00595a59921f653a2e8bb427abcc7
AndrewCandy <adecandia@olin.edu>
2022-03-30 13:59:34 -0400

    Successfull pmaw pull, needs refactor.

--- reddit/pmaw_api.py
+++ reddit/pmaw_api.py
@@ -1,3 +1,4 @@
+from matplotlib.pyplot import title
 import pandas as pd
 from pmaw import PushshiftAPI
 import datetime
@@ -26,7 +27,7 @@ end_timestamp = int(datetime.datetime(
     year=2019, month=1, day=1).timestamp())
 
 subreddit="wallstreetbets"
-limit=300
+limit=250000
 
 submissions = api.search_submissions(subreddit=subreddit, limit=limit,
                         before=end_timestamp, after=beginning_timestamp)
@@ -37,24 +38,32 @@ subs_df = pd.DataFrame(submissions)
 print(len(subs_df))
 
 subs_df = subs_df[['title', 'selftext', 'created_utc']]
-print(subs_df.columns)
-print(subs_df.head(5))
 
-for i, post in subs_df.iterrows():
+# Init dataframe
+df = pd.DataFrame()
+index = -1
+for post in subs_df.itertuples():
+ 
+    title = str(post[1])
+    selftext = str(post[2])
+    created_utc = post[3]
 
-
-if 'selftext' in post and (findall_tickers(post['title']) or \
-                            findall_tickers(post['selftext'])):
-
-    censored_title = post['title']  #pf.censor()
-    censored_selftext = post['selftext']
-    time = datetime.datetime.fromtimestamp(post['created_utc'])
-    ticker_list = [findall_tickers(post['title'] + post['selftext'])]
+    if selftext != "" and (findall_tickers(title) or \
+                            findall_tickers(selftext)):
+        
+        index += 1
+        #censored_title = post['title']  #pf.censor()
+        #censored_selftext = post['selftext']
+        time = datetime.datetime.fromtimestamp(created_utc)
+        ticker_list = findall_tickers(title + " " + selftext)
         #ticker_list = (findall_tickers(post['title'])).extend(
         #               findall_tickers(post['selftext']))
 
-    df = pd.concat(
-        [df, pd.DataFrame({'title': [censored_title],
-                           'selftext': [censored_selftext],
-                           'time': [time],
-                           'tickers': ticker_list})])
+        df = pd.concat(
+            [df, pd.DataFrame({'index' : [index],
+                            'title': [title],
+                            'selftext': [selftext],
+                            'time': [time],
+                            'tickers': [ticker_list]})])
+
+df.to_csv("newapi.csv")
