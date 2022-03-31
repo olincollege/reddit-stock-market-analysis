bd4616029fbc511b7c078eaae946087165d9b7d9
Shamama Sirroon <ssirroon@olin.edu>
2022-03-31 01:58:42 -0400

    Fix repeats

--- reddit_to_stock.py
+++ reddit_to_stock.py
@@ -14,16 +14,27 @@ all_tickers = df['tickers']
 # list_title = title.tolist()
 
 
-existing_tickers = []
+def remove_dupes(ticker_list):
+    """
+    removes empty strings and repeats
+    """
+    res = []
+    [res.append(x) for x in ticker_list if x not in res]
+    if '' in res:
+        res.remove('')
+    return res
+
+
+existing_tickers = ['SPY']
 for post in df.itertuples():
     index = post[2]
     title = str(post[3])
     selftext = str(post[4])
     time = str(post[5])[0:10]
-    tickers = post[6]
+    tickers = str(post[6][2:-3]).split("', '")
     if not re.findall(r'[Ll]ong', selftext + ' ' + title):
-        print(type(tickers))
-        print(tickers)
+        tickers = remove_dupes(tickers)
+        df.iat[index, 5] = [tickers]
         for single_ticker in tickers:
             if single_ticker not in existing_tickers:
                 existing_tickers.append(single_ticker)
@@ -31,8 +42,7 @@ for post in df.itertuples():
                 tickers.remove(single_ticker)
         if not tickers:
             df.drop(labels=[index], axis=0, inplace=True)
-        else:
-            df.iat[index, 5] = [tickers]
+
 
 print(len(df))
 
