e3231072040012ef97a5922e459f17f2bc39c76c
Emma Fox <efox@olin.edu>
2022-04-03 15:54:00 -0400

    Refactor and add unit tests for pull_stock_info
    Delete pullstockinfo.py, which has been renamed.

--- test_functions.py
+++ test_functions.py
@@ -6,6 +6,15 @@ from stock_info.pull_stock_info import get_datetime
 
 # Define sets of test cases.
 get_datetime_cases = [
-    # Check that the complement of A is T.
-    ("A", "T"),
+    # Check that a simple addition case returns a datetime.date object.
+    ("2022-01-01", 10, [datetime.date(2022, 1, 1), 
+                        datetime.date(2022, 1, 11)]),
+    # Check addition for months.
+    ("2022-01-01", 32, [datetime.date(2022, 1, 1), 
+                        datetime.date(2022, 2, 1)]),
+    # Check addition for years.
+    ("2021-01-01", 365, [datetime.date(2021, 1, 1), 
+                        datetime.date(2022, 1, 1)]),
 ]
+
+
