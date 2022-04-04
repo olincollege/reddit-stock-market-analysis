85782d1dd135982344f42d346243d5629bff3fb8
AndrewCandy <adecandia@olin.edu>
2022-04-04 00:11:54 -0400

    Added testcases for find_tickers.

--- test_functions.py
+++ test_functions.py
@@ -1,3 +1,4 @@
+import datetime
 import pytest
 
 # Scraping and analyzing Alpaca data
@@ -5,12 +6,23 @@ import pytest
 from stock_info.pull_stock_info import get_datetime
 
 # TODO: pmaw_api find tickers
+find_tickers_cases = [
+    # Check that a string with no tickers returns an empty list.
+    ("something with words", []),
+    # Check that capital letters without a dollar sign are not counted.
+    ("CAPITAL LETTERS", []),
+    # Check that a valid ticker symbol is recorded
+    ("Words$TSLA more wWORds", ['TSLA']),
+    # Check that multiple ticker symbols are recorded
+    ("Long $TSLA & $AAPLto be cool", ['TSLA', 'APPL']),
+]
+
+
 # TODO: pmaw_api find_qmarks
 # TODO: pmaw_api find_long
 # TODO: pmaw_api find_short
 # TODO: pmaw_api str_create_timestamp
 # TODO: pmaw_api remove_dupes
-# TODO: pull_stock_info get_datetime
 # TODO: graph_stock_info days_since_epoch
 # TODO: graph_stock_info date_from_epoch_time
 # TODO: generate_results str_to_list
@@ -19,7 +31,7 @@ from stock_info.pull_stock_info import get_datetime
 
 
 
-# Define sets of test cases.
+# TODO: pull_stock_info get_datetime
 get_datetime_cases = [
     # Check that a simple addition case returns a datetime.date object.
     ("2022-01-01", 10, [datetime.date(2022, 1, 1), 
